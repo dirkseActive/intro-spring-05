f718acbddeeb41386f70647da75d33e6f534f2d1
dirkseActive <odysseusontheocean@yahoo.com>
2017-03-13 17:14:55 -0400

    last commit

---
+++ src/main/java/com/apress/isf/spring/aop/BeforeLoggingModule.java
@@ -0,0 +1,29 @@
+package com.apress.isf.spring.aop;
+
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+/**
+ * 
+ * @author dA
+ * @since 3/10/207
+ */
+
+public class BeforeLoggingModule implements MethodBeforeAdvice {
+	private static final Logger log = LoggerFactory.getLogger(BeforeLoggingModule.class);
+	
+	public void before(Method method, Object[] args, Object target) throws Throwable {
+		if(log.isDebugEnabled()){
+			log.debug("@@@(BEFORE) Method Called: " + method.getName());
+			if(args.length == 0)
+				log.debug("@@@(BEFORE) No arguments passed.");
+			for(Object arg:args)
+				log.debug("@@@(BEFORE) Argument passed:" + arg);
+			}
+		}
+	}
+	
